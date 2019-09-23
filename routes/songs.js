9030e6c502b0bd6d314cd577ccb0d2be9ad278eb
Ducky <s0566788@htw-berlin.de>
2019-09-23 22:20:22 +0200

    http calls added

--- routes/songs.js
+++ routes/songs.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const bodyParser =  require('body-parser');
+const bodyParser = require('body-parser');
 const router = express.Router();
 const cors = require('cors');
+const {checkSchema} = require('express-validator');
 
+
+router.use(cors());
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded());
-router.use(cors());
 
-router.get('/songs', function (req, res) {
 let songs = [
     {
         id: 1,
@@ -39,8 +40,64 @@ let songs = [
 
     }
 ];
-    res.status(200).send(songs);
+
+function remove (array, id){
+    return array.filter(song => song.id!==id);
+}
+
+router.all('*', function (req, res, next) {
+    console.log('song requested');
+    next();
+})
+
+router.get('/songs', function (req, res) {
+    res.send(songs);
 });
 
+router.get('/songs/:songId', function (req, res) {
+    let data = songs.find(song =>
+        song.id === parseInt(req.params.songId)
+    );
+    res.status(200).send(data);
+});
 
+router.post('/songs',
+
+    /*checkSchema({
+    title: {
+        in: ["body"],
+        isString: true,
+        rtrim: {
+            options:[[" "]]
+        }
+    }
+
+
+})
+,*/function (req, res) {
+
+        let song = req.body;
+        newId = songs.length + 1;
+        song.id = newId;
+
+        songs.push(song);
+        console.log(song);
+        res.status(201).send(song);
+    });
+
+router.put('/songs/:songId', function (req, res, next) {
+    const songId = parseInt(req.params.songId);
+    for (let i in songs){
+        if (songs[i].id === songId){
+            songs[i] = req.body;
+            songs[i].id = parseInt(songs[i].id );
+        }
+    }
+    res.status(200).send();
+});
+
+router.delete('/songs/:songId', function (req, res) {
+    songs = remove(songs,parseInt(req.params.songId));
+    res.status(200).send();
+});
 module.exports = router;
